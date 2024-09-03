ce45329b05a51debfc02dbb4eb39b477f795950f
Didi518 <kevin.ouali@gmail.com>
2024-09-03 20:03:11 +0200

    :busts_in_silhouette: workspace members

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -3,6 +3,18 @@ import { getAuthUserId } from "@convex-dev/auth/server";
 
 import { mutation, query } from "./_generated/server";
 
+const generateCode = (length = 12, includeSpecialChars = true) => {
+  let chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (includeSpecialChars) {
+    chars += "!@#$%^&*()_+-=[]{}|;:,.<>?";
+  }
+
+  return Array.from(
+    { length },
+    () => chars[Math.floor(Math.random() * chars.length)]
+  ).join("");
+};
+
 export const create = mutation({
   args: {
     name: v.string(),
@@ -14,13 +26,18 @@ export const create = mutation({
       throw new Error("Non autorisé");
     }
 
-    // TODO: créer une méthode
-    const joinCode = "123456";
+    const joinCode = generateCode();
 
     const workspaceId = await ctx.db.insert("workspaces", {
       name: args.name,
       userId,
       joinCode,
+    });
+
+    await ctx.db.insert("members", {
+      userId,
+      workspaceId,
+      role: "admin",
     });
 
     return workspaceId;
@@ -30,7 +47,30 @@ export const create = mutation({
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query("workspaces").collect();
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      return [];
+    }
+
+    const members = await ctx.db
+      .query("members")
+      .withIndex("by_user_id", (q) => q.eq("userId", userId))
+      .collect();
+
+    const workspaceIds = members.map((member) => member.workspaceId);
+
+    const workspaces = [];
+
+    for (const workspaceId of workspaceIds) {
+      const workspace = await ctx.db.get(workspaceId);
+
+      if (workspace) {
+        workspaces.push(workspace);
+      }
+    }
+
+    return workspaces;
   },
 });
 
@@ -41,6 +81,17 @@ export const getById = query({
 
     if (!userId) {
       throw new Error("Non autorisé");
+    }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.id).eq("userId", userId)
+      )
+      .unique();
+
+    if (!member) {
+      return null;
     }
 
     return await ctx.db.get(args.id);
