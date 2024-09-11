309dae6255ba4acb59d16cb3b6f231a0d1ed6501
Didi518 <kevin.ouali@gmail.com>
2024-09-11 06:54:13 +0200

    :monocle_face: search dialog component

--- src/app/espace-de-travail/[workspaceId]/membre/[memberId]/conversation.tsx
+++ src/app/espace-de-travail/[workspaceId]/membre/[memberId]/conversation.tsx
@@ -4,6 +4,7 @@ import { useGetMember } from "@/features/members/api/use-get-member";
 import { useGetMessages } from "@/features/messages/api/use-get-messages";
 
 import { useMemberId } from "@/hooks/use-member-id";
+import { usePannel } from "@/hooks/use-pannel";
 import { MessageList } from "@/components/message-list";
 
 import { ChatInput } from "./chat-input";
@@ -17,6 +18,7 @@ interface ConversationProps {
 
 export const Conversation = ({ id }: ConversationProps) => {
   const memberId = useMemberId();
+  const { onOpenProfile } = usePannel();
   const { data: member, isLoading: memberLoading } = useGetMember({
     id: memberId,
   });
@@ -35,7 +37,7 @@ export const Conversation = ({ id }: ConversationProps) => {
       <Header
         memberName={member?.user.name}
         memberImage={member?.user.image}
-        onClick={() => {}}
+        onClick={() => onOpenProfile(memberId)}
       />
       <MessageList
         data={results}
